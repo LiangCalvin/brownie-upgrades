9fe42764a4607ee2a02212f91ee4137f8ba77ee4
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-08-02 14:10:11 +0700

    add helpful script

---
+++ scripts/helpful_scritps.py
@@ -0,0 +1,25 @@
+from brownie import accounts, network, config
+import eth_utils
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local", "mainnet-fork"]
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+    return accounts.add(config["wallets"]["from_key"])
+
+
+# initializer=box.store,1 , ...
+def encode_function_data(initializer=None, *args):
+    if len(args) == 0 or not initializer:
+        return eth_utils.to_bytes(hexstr="0x")
+    return initializer.encode_input(*args)

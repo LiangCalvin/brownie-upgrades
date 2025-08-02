cc113f46277b19d129df187e00cb7c0c7b80a0ae
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-08-02 14:10:19 +0700

    add deploy script

---
+++ scripts/deploy_and_upgrade.py
@@ -0,0 +1,26 @@
+from scripts.helpful_scritps import get_account, encode_function_data
+from brownie import network, Box, ProxyAdmin, TransparentUpgradeableProxy, Contract
+
+
+def main():
+    account = get_account()
+    print(f"Deploying to {network.show_active()}")
+    box = Box.deploy({"from": account})
+    print(box.retrieve())
+
+    proxy_admin = ProxyAdmin.deploy({"from": account})
+
+    # initializer = box.store, 1
+    box_encoded_initializer_function = encode_function_data()
+
+    proxy = TransparentUpgradeableProxy.deploy(
+        box.address,
+        proxy_admin.address,
+        box_encoded_initializer_function,
+        {"from": account, "gas_limit": 1000000},
+    )
+    print(f"Proxy deploy to {proxy}, you can now upgrade to V2!")
+
+    proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
+    proxy_box.store(1, {"from": account})
+    print(proxy_box.retrieve())
